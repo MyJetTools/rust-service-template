f9a88a34335248c8b9833f8dc71e8810906dc38c
Aleksandr Belousov <aleksandr.b@smplt.net>
2022-07-09 23:16:27 +0300

    reading complete

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,13 +1,10 @@
-use serde::{de::DeserializeOwned, Serialize};
+use serde::{de::DeserializeOwned, Serialize, Deserialize};
 use tokio::{fs::File, io::AsyncReadExt};
 
 pub struct SettingsReader {}
 
 impl SettingsReader {
-    pub async fn read_settings<T>() -> Result<T, ()>
-    where
-        T: DeserializeOwned + Serialize,
-    {
+    pub async fn read_settings<T>() -> Result<T, ()> where T: DeserializeOwned {
         if let Ok(result) = read_from_url::<T>().await {
             return Ok(result);
         }
@@ -20,36 +17,31 @@ impl SettingsReader {
     }
 }
 
-pub async fn read_from_url<T>() -> Result<T, ()>
-    where
-        T: DeserializeOwned + Serialize,
-    {
-        let settings_url = std::env::var("SETTINGS_URL".to_string());
+pub async fn read_from_url<T>() -> Result<T, ()> where T: DeserializeOwned {
+    let settings_url = std::env::var("SETTINGS_URL".to_string());
 
-        match settings_url {
-            Ok(res) => {
-                let client = reqwest::Client::new();
-                let response_result = client.get(res).send().await;
-                match response_result {
-                    Ok(response) => {
-                        let bytes = response.bytes().await.unwrap();
-                        let data = bytes.to_vec();
-                        let result: T = serde_yaml::from_slice(&data).unwrap();
-                        return Ok(result);
-                    },
-                    Err(_) => println!("Settings url is not set!"),
+    match settings_url {
+        Ok(res) => {
+            let client = reqwest::Client::new();
+            let response_result = client.get(res).send().await;
+            match response_result {
+                Ok(response) => {
+                    let bytes = response.bytes().await.unwrap();
+                    let data = bytes.to_vec();
+                    println!("{:?}", data);
+                    let result: T = serde_yaml::from_slice(&data).unwrap();
+                    return Ok(result);
                 }
+                Err(_) => println!("Settings url is not set!"),
             }
-            Err(_) => println!("Settings url is not set!"),
         }
-
-        read_from_file::<T>().await
+        Err(_) => println!("Settings url is not set!"),
     }
 
-async fn read_from_file<T>() -> Result<T, ()>
-where
-    T: DeserializeOwned + Serialize,
-{
+    read_from_file::<T>().await
+}
+
+async fn read_from_file<T>() -> Result<T, ()> where T: DeserializeOwned {
     let filename = get_settings_filename();
 
     println!("Reading settings file {}", filename);
@@ -92,4 +84,32 @@ fn get_settings_filename() -> String {
     let home_path = env!("HOME");
     let filename = format!("{}/{}", home_path, ".settings");
     filename
+}
+
+#[cfg(test)]
+mod tests {
+    use serde::{de::DeserializeOwned, Deserialize, Serialize};
+
+    use super::SettingsReader;
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct SettingsModel {
+        #[serde(rename = "RustServiceTemplateTest")]
+        pub inner: SettingsModelInner,
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct SettingsModelInner {
+        #[serde(rename = "ZipkinUrl")]
+        pub zipkin_url: String,
+
+        #[serde(rename = "SeqServiceUrl")]
+        pub seq_service_url: String,
+    }
+
+    #[tokio::test]
+    async fn test_something_async() {
+        let settings = SettingsReader::read_settings::<SettingsModel>().await.unwrap();
+        println!("{:?}", settings);
+    }
 }
